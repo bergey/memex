835fb8fe74a11d5316a75beac3a2f07dff68d99c
Daniel Bergey <bergey@teallabs.org>
2024-12-15 14:30:35 +0000

    askama template init

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use askama::Template;
 use axum::{routing::get, Router};
-use tokio;
+use tokio; // bring trait in scope
+
+#[derive(Template)] // this will generate the code...
+#[template(path = "hello.html")] // using the template in this path, relative
+                                 // to the `templates` dir in the crate root
+struct HelloTemplate<'a> {
+    // the name of the struct can be anything
+    name: &'a str, // the field name should match the variable name
+                   // in your template
+}
 
 #[tokio::main]
 async fn main() {
+
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async {
+        let hello = HelloTemplate { name: "bergey" }; // instantiate your struct
+        hello.render().unwrap()
+    }));
 
     // run our app with hyper, listening globally on port 8000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
