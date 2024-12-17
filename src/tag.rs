8499e9e0a483971bc9a2f2e77953979eee5c8ae5
Daniel Bergey <bergey@teallabs.org>
2024-12-17 14:01:59 +0000

    separating conjunction [wip]

--- src/tag.rs
+++ src/tag.rs
@@ -1,4 +1,4 @@
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone, Copy)]
 pub struct TagId(u16);
 
 pub struct Tag {
@@ -7,12 +7,50 @@ pub struct Tag {
 }
 
 pub enum Query {
+    Empty,
     Tag(TagId),
+    And(&Query, &Query),
+    Or(&Query, &Query),
 }
 
-pub fn match_tags(query: Query, tags: Vec<TagId>) -> bool {
+// TODO benchmark BTreeSet, HashSet, persistent immutable tree
+
+pub fn match_tags(query: &Query, orig_tags: &[TagId]) -> bool {
+    let mut tags = Vec::new();
+    tags.extend_from_slice(orig_tags);
+    match_tags_mut(query, &mut tags)
+}
+
+fn match_tags_mut(query: &Query, tags: &mut Vec<TagId>) -> bool {
     match query {
-        Query::Tag(id) => tags.contains(&id)
+        Query::Empty => tags.is_empty(),
+        Query::Tag(id) => remove(&mut tags, id),
+        Query::And(q1, q2) => match_tags_mut(q1, &mut tags) && match_tags_mut(q2, &mut tags),
+        Query::Or(q1, q2) => {
+            let mut tags_copy = tags.clone();
+            if match_tags_mut(query, &mut tags_copy) {
+                std::mem::replace(tags, tags_copy);
+                true
+            } else {
+                let mut tags_copy = tags.clone();
+                if match_tags_mut(query, &mut tags_copy) {
+                    std::mem::replace(tags, tags_copy);
+                    true
+                } else {
+                    false
+                }
+            }
+        }
     }
 }
-    
+
+/// return true if element was found
+fn remove(tags: &mut Vec<TagId>, t: TagId) -> bool {
+    for i in 0..tags.len() {
+        if tags[i] == t {
+            tags.swap_remove(i);
+            return true;
+        }
+    }
+    return false;
+}
