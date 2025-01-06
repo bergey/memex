5bdde7f65ba31f2915406db4330ca6511f10d5a9
Daniel Bergey <bergey@teallabs.org>
2025-01-06 02:36:11 +0000

    refactor: add zotero module

--- src/bin/memex.rs
+++ src/bin/memex.rs
@@ -1,26 +1,12 @@
 // memex cli
 
 use clap::Parser;
-use futures_util::TryStreamExt;
-use sqlx::sqlite::SqlitePool;
-use sqlx::Row;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use winnow::Parser as WParser;
 
 use memex::tag::query::*;
 use memex::tag::*;
-
-#[derive(Eq, Hash, PartialEq, Debug)]
-enum DocId {
-    Zotero(i64),
-    // Calibre(i64),
-}
-
-struct Doc {
-    // id: DocId,
-    title: String,
-    tags: Tags,
-}
+use memex::{Doc, DocId};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -34,58 +20,11 @@ struct Cli {
 #[tokio::main()]
 async fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
-    let pool = SqlitePool::connect("/home/bergey/Zotero/zotero.sqlite").await?;
-    let mut conn = pool.acquire().await?;
 
     let mut all_tags = AllTags::new();
     let mut docs: HashMap<DocId, Doc> = HashMap::new();
 
-    // assign our own IDs, so we can merge tags from multiple libraries
-    let zot_tags = {
-        let mut zot_tags: HashMap<i64, TagId> = HashMap::new();
-        let mut rows = sqlx::query("select tagId, name from tags").fetch(&mut *conn);
-        while let Some(row) = rows.try_next().await? {
-            let zid = row.try_get("tagID")?;
-            let name = row.try_get("name")?;
-            let memex_id = all_tags.insert(name);
-            zot_tags.insert(zid, memex_id);
-        }
-        zot_tags
-    };
-
-    // titles
-    {
-        // TODO emails have subject, not title
-        // exclude attachments (even if orphaned)
-        let mut rows = sqlx::query("select itemID, value as title from items join itemData using (itemID) join fieldsCombined using (fieldID) join itemDataValues using (valueID) where fieldName = 'title' and itemTypeID != 3").fetch(&mut *conn);
-        while let Some(row) = rows.try_next().await? {
-            let zid = row.try_get("itemID")?;
-            let title = row.try_get("title")?;
-            docs.insert(
-                DocId::Zotero(zid),
-                Doc {
-                    title: title,
-                    tags: HashSet::new(),
-                },
-            );
-        }
-    }
-
-    // tag the documents
-    let mut rows = sqlx::query("select itemID, tagID from itemTags").fetch(&mut *conn);
-    while let Some(row) = rows.try_next().await? {
-        let item_id = row.try_get("itemID")?;
-        let z_tag_id = row.try_get("tagID")?;
-        let memex_tag_id = zot_tags
-            .get(&z_tag_id)
-            .ok_or(anyhow::anyhow!("tag ID violates foreign key constraint"))?;
-        match docs.get_mut(&DocId::Zotero(item_id)) {
-            Some(doc) => {
-                doc.tags.insert(memex_tag_id.clone());
-            }
-            None => (), //println!("skipping doc ID {item_id} has no title"),
-        };
-    }
+    memex::zotero::load_docs(&mut all_tags, &mut docs).await?;
 
     println!("{} tags", all_tags.len());
     println!("{} titles", docs.len());
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,14 @@
 // pub mod note;
 pub mod tag;
+pub mod zotero;
 
-use std::collections::HashSet;
+#[derive(Eq, Hash, PartialEq, Debug)]
+pub enum DocId {
+    Zotero(i64),
+    // Calibre(i64),
+}
 
 pub struct Doc {
     pub title: String,
     pub tags: tag::Tags,
 }
-
-pub fn fake_documents() -> Vec<Doc> {
-    use tag::TagId;
-
-    let mut ret = Vec::new();
-
-    let tags = HashSet::from([TagId(1)]);
-    ret.push(Doc { title: "A".to_string(), tags });
-
-    let tags = HashSet::from([TagId(1), TagId(2)]);
-    ret.push(Doc { title: "B".to_string(), tags });
-
-    let tags = HashSet::from([TagId(1), TagId(2), TagId(3)]);
-    ret.push(Doc { title: "C".to_string(), tags });
-
-    let tags = HashSet::from([]);
-    ret.push(Doc { title: "D".to_string(), tags });
-
-    ret
-}
-
