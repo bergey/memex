84957969c949ab44187f2ea374578d290f3e186e
Daniel Bergey <bergey@teallabs.org>
2026-05-25 07:44:06 -0400

    render: split by section

--- client/src/render.rs
+++ client/src/render.rs
@@ -2,20 +2,8 @@ use leptos::prelude::*;
 
 pub fn body() -> impl IntoView {
     view! {
-        <section id="search">
-        <h1>Search</h1>
-        <menu>
-            <li>Craft</li>
-            <li>Science</li>
-            <li>Society</li>
-            <li>Software</li>
-        </menu>
-        </section>
+        <Search />
         <section id="list">
-        <h1>List</h1>
-        <p>
-            There should be a table of books / documents here.
-        </p>
         <table>
             <thead>
             <tr>
@@ -32,15 +20,35 @@ pub fn body() -> impl IntoView {
             </tr>
         </table>
         </section>
+        <Details />
+    }
+}
+
+fn Search() -> impl IntoView {
+    view! {
+        <section id="search">
+            <h1>Search</h1>
+            <menu>
+                <li>Craft</li>
+                <li>Science</li>
+                <li>Society</li>
+                <li>Software</li>
+            </menu>
+        </section>
+    }
+}
+
+fn Details() -> impl IntoView {
+    view! {
         <section id="details">
-        <h1>Details</h1>
-        <ul>
-            <li><label>Title <input id="title" type="text" value="The Dawn of Everything" /></label></li>
-            <li><label>Author <input id="author" type="text" value="David Graeber" /></label></li>
-            <li><label>Date <input id="date" type="text" /></label></li>
-            <li><label>Publisher <input id="publisher" type="text" /></label></li>
-            <li><label>URL <input id="url" type="text" /></label></li>
-        </ul>
+            <h1>Details</h1>
+            <ul>
+                <li><label>Title <input id="title" type="text" value="The Dawn of Everything" /></label></li>
+                <li><label>Author <input id="author" type="text" value="David Graeber" /></label></li>
+                <li><label>Date <input id="date" type="text" /></label></li>
+                <li><label>Publisher <input id="publisher" type="text" /></label></li>
+                <li><label>URL <input id="url" type="text" /></label></li>
+            </ul>
         </section>
     }
 }
