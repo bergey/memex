c32983b97c96432574fa7a13b051848d8784d5ab
Daniel Bergey <bergey@teallabs.org>
2026-05-29 15:21:53 -0400

    PWA manifest, service worker

--- client/public/main.js
+++ client/public/main.js
@@ -5,3 +5,28 @@ import init, { start } from "../.wasm_pack/client.js";
   start("ws://localhost:3003/ws");
 })()
 
+async function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        new URL("./service-worker.ts", import.meta.url),
+        {
+          scope: "/",
+          type: "module",
+        }
+      );
+      if (process.env.NODE_ENV !== "production") {
+        if (registration.installing) {
+          console.log("Service worker installing");
+        } else if (registration.waiting) {
+          console.log("Service worker installed");
+        } else if (registration.active) {
+          console.log("Service worker active");
+        }
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+}
+registerServiceWorker();
