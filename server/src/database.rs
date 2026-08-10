1b6ffc2d272935f7c88dc9815ecd30754f11a39f
Daniel Bergey <bergey@teallabs.org>
2026-08-10 16:34:18 -0400

    insert first drawing

--- server/src/database.rs
+++ server/src/database.rs
@@ -14,7 +14,8 @@ pub async fn read_library(transaction: &mut Transaction<'_, Postgres>, id: Uuid)
 
 pub async fn save_library(transaction: &mut Transaction<'_, Postgres>, id: Uuid, library: &mut AutoCommit) -> anyhow::Result<()> {
     let bytes = library.save();
-    query!("update libraries set value = $2 where id = $1", id, bytes)
+    query!("insert into libraries (id, value) values ($1, $2) \
+on conflict (id) do update set value = $2", id, bytes)
         .execute(&mut **transaction)
         .await?;
     Ok(())
