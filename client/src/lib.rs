f1d3536370572f051a31e71c5667e4ce0e6e3800
Daniel Bergey <bergey@teallabs.org>
2026-08-11 07:22:08 -0400

    client: tracing, not log, set up console

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -8,21 +8,14 @@ use automerge::sync::SyncDoc;
 use prelude::*;
 
 use futures::select;
-use log::Level;
 use std::panic;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 
-#[cfg(debug_assertions)]
-const LOG_LEVEL: Level = Level::Debug;
-
-#[cfg(not(debug_assertions))]
-const LOG_LEVEL: Level = Level::Info;
-
 #[wasm_bindgen]
 pub fn start(server_ws_url: Option<String>) -> Result<()> {
-    panic::set_hook(Box::new(console_error_panic_hook::hook));
-    console_log::init_with_level(LOG_LEVEL).expect("failed to init logging");
+    console_error_panic_hook::set_once();
+    wasm_tracing::set_as_global_default();
 
     let (tx_a, mut rx_a) = Sender::new(3);
     let (mut tx_e, mut rx_e) = Sender::new(3);
