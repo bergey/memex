486dcea2bcb147e2e2f9adb425ad0e175951ac0f
Daniel Bergey <bergey@teallabs.org>
2026-08-13 12:35:52 -0400

    comment out unused Error struct

--- server/src/prelude.rs
+++ server/src/prelude.rs
@@ -1,6 +1,6 @@
 use axum::extract::State;
-use axum::http::StatusCode;
-use axum::response::{IntoResponse, Response};
+// use axum::http::StatusCode;
+// use axum::response::{IntoResponse, Response};
 use sqlx::PgPool;
 pub use tracing::*;
 
@@ -13,27 +13,28 @@ pub struct ConnectionPools {
 
 // TODO trait to make State wrapper less intrusive?
 
+// maybe I will want this if I add endpoints beyond the Websocket?
 // https://github.com/tokio-rs/axum/blob/main/examples/anyhow-error-response/src/main.rs
-pub struct Error {
-    error: anyhow::Error,
-    status_code: StatusCode,
-}
+// pub struct Error {
+//     error: anyhow::Error,
+//     status_code: StatusCode,
+// }
 
-impl IntoResponse for Error {
-    fn into_response(self) -> Response {
-        error!("{}", self.error);
-        (self.status_code, format!("Something went wrong")).into_response()
-    }
-}
+// impl IntoResponse for Error {
+//     fn into_response(self) -> Response {
+//         error!("{}", self.error);
+//         (self.status_code, format!("Something went wrong")).into_response()
+//     }
+// }
 
-impl<E> From<E> for Error
-where
-    E: Into<anyhow::Error>,
-{
-    fn from(err: E) -> Self {
-        Self {
-            error: err.into(),
-            status_code: StatusCode::INTERNAL_SERVER_ERROR,
-        }
-    }
-}
+// impl<E> From<E> for Error
+// where
+//     E: Into<anyhow::Error>,
+// {
+//     fn from(err: E) -> Self {
+//         Self {
+//             error: err.into(),
+//             status_code: StatusCode::INTERNAL_SERVER_ERROR,
+//         }
+//     }
+// }
