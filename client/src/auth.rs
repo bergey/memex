8b078b665278e83ee3877bc9f4b74d4412932cec
Daniel Bergey <bergey@teallabs.org>
2026-08-24 15:49:11 -0400

    local auth_token in base64

--- client/src/auth.rs
+++ client/src/auth.rs
@@ -1,17 +1,12 @@
 use crate::prelude::*;
 use memex_shared::AuthToken;
 
-use std::str::FromStr;
-use uuid::Uuid;
 use wasm_bindgen::prelude::*;
-use web_sys as browser;
-// use webauthn_rs_proto::attest as server;
 
 pub fn load_auth_token() -> Option<AuthToken> {
     let storage = local_storage()?;
     let s = storage.get_item("auth_token").log_error().flatten()?;
-    let uuid = Uuid::from_str(&s).ok()?; // TODO store in base-64 locally
-    Some(AuthToken::from_uuid(&uuid))
+    AuthToken::from_str(s.as_ref())
 }
 
 pub fn save_auth_token(_auth_token: AuthToken) {
@@ -21,10 +16,6 @@ pub fn save_auth_token(_auth_token: AuthToken) {
 // where should User ID / name come from?
 pub fn request_auth_token() -> Result<AuthToken> {
     panic!("not implemented");
-}
-
-fn credentials() -> Option<browser::CredentialsContainer> {
-    web_sys::window().map(|window| window.navigator().credentials())
 }
 
 #[wasm_bindgen(module = "/src/auth.js")]
