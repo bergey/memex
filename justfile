10a882a8d92e6ed099049fe5c8fe965d1125c5bc
Daniel Bergey <bergey@teallabs.org>
2026-08-31 09:45:19 -0400

    deploy: justfile, sql migrations, env vars

--- justfile
+++ justfile
@@ -3,3 +3,25 @@ update:
 
 sqlx-cli:
     cargo install sqlx-cli --no-default-features --features rustls,postgres
+
+setup:
+ createuser memex --login || true
+ createdb {{pg_db}} -O memex || true
+ psql -d {{pg_db}} -c 'grant all on database "{{pg_db}}" to memex' -c 'grant all on all tables in schema public to memex' -c 'grant all on schema public to memex'
+
+release:
+    cargo build --release --bin server
+    just client/release
+
+deploy: release
+    sudo cp target/release/server /usr/bin/memex-server --backup=numbered
+    sudo systemctl restart memex
+    # server should never be older than client
+    sudo cp -r client/dist/. /usr/share/caddy/memex --backup=numbered
+
+# after verifying deploy worked
+deploy-clean:
+    rm /usr/share/caddy/memex/*~
+    rm /usr/bin/memex-server*~
+
+pg_db := "memex-dev"
--- server/src/main.rs
+++ server/src/main.rs
@@ -9,6 +9,7 @@ use prelude::*;
 
 use axum::routing::{get, post};
 use sqlx::postgres::PgPoolOptions;
+use std::env;
 
 #[macro_use]
 extern crate lazy_static;
@@ -20,9 +21,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let connection_pools = ConnectionPools {
         postgres: PgPoolOptions::new()
             .max_connections(5)
-            .connect("postgres://memex:memex@localhost:5432/memex")
+            .connect(&env::var("DATABASE_URL").unwrap_or("postgres://memex:memex@localhost:5432/memex".to_string()))
             .await?,
     };
+    sqlx::migrate!().run(&connection_pools.postgres).await?;
 
     let app = axum::Router::new()
         .route("/ws", get(websocket::ws_upgrade))
@@ -31,9 +33,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .route("/login/{user_id}/start", get(passkeys::login_start))
         .route("/login/finish", post(passkeys::login_finish))
         .with_state(connection_pools);
-    // TODO serve client code
 
-    let addr = "0.0.0.0:3036"; // TODO env var
+    // TODO require env var in release build
+    let addr = env::var("LISTEN").unwrap_or("0.0.0.0:3036".to_string());
     info!("memex listening on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await?;
 
