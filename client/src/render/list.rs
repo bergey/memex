f933293db195d114c79fba1c7bb91fec5c8fe8b7
Daniel Bergey <bergey@teallabs.org>
2026-08-31 13:50:45 -0400

    parse query from search field

--- client/src/render/list.rs
+++ client/src/render/list.rs
@@ -1,9 +1,13 @@
+use super::Record;
 use crate::prelude::*;
 use memex_shared::library::action::Action;
+
+use memex_query::*;
 
 use leptos::html::*;
 use leptos::prelude::*;
 use leptos::tachys::html::event;
+use std::collections::HashSet;
 
 pub fn list_section(library: super::ReactiveLibrary, tx: Sender<Action>) -> impl IntoView {
     let mut library_add_button = tx.clone();
@@ -23,29 +27,13 @@ pub fn list_section(library: super::ReactiveLibrary, tx: Sender<Action>) -> impl
                 // each: move || library.records.get(),
                 each: move || {
                     let search = library.search.get();
-                    if search.is_empty() {
-                        library.records.get()
-                    } else {
-                        library.records.with(|records| {
-                            let mut ret = Vec::new();
-                            for r in records {
-                                r.tags.with(|tags| {
-                                    let mut found = false;
-                                    for t in tags {
-                                        t.name.with(|name| {
-                                            if name == &search {
-                                                ret.push(r.clone());
-                                                found = true;
-                                            }
-                                        });
-                                        if found {
-                                            break;
-                                        }
-                                    }
-                                })
-                            }
-                            ret
-                        })
+                    match parse_query(&*search) {
+                        Err(e) => {
+                            warn!("{e}");
+                            library.records.get()
+                        }
+                        // TODO memoize, handle invalid queries
+                        Ok(query) => filter_library(query, &library.records),
                     }
                 },
                 key: |record| record.id.clone(),
@@ -70,4 +58,26 @@ pub fn list_section(library: super::ReactiveLibrary, tx: Sender<Action>) -> impl
             }),
         )),
     ))
+}
+
+fn filter_library(query: Query<String>, records: &RwSignal<Vec<Record>>) -> Vec<Record> {
+    let mut ret = Vec::new();
+    records.with(|records| {
+        for r in records {
+            // TODO don't rebuild this set every time the query changes
+            let mut tag_set = HashSet::new();
+            r.tags.with(|tags| {
+                for t in tags {
+                    let name = t.name.get();
+                    if !name.is_empty() {
+                        tag_set.insert(name);
+                    }
+                }
+            });
+            if match_tags(&query, &tag_set) {
+                ret.push(r.clone());
+            }
+        }
+    });
+    ret
 }
