0ee9db21e7cbdb49c2c0eb03d1fc022fb6b978f4
Daniel Bergey <bergey@teallabs.org>
2026-09-08 13:17:49 -0400

    save_auth_token

--- client/src/auth.rs
+++ client/src/auth.rs
@@ -5,13 +5,23 @@ use wasm_bindgen::prelude::*;
 
 pub fn load_auth_token() -> Option<AuthToken> {
     let storage = local_storage()?;
-    let s = storage.get_item("auth_token").log_error("get auth_token").flatten()?;
+    let s = storage
+        .get_item("auth_token")
+        .log_error("get auth_token")
+        .flatten()?;
     AuthToken::from_str(s.as_ref())
 }
 
 #[allow(dead_code)]
-pub fn save_auth_token(_auth_token: AuthToken) {
-    panic!("not implemented");
+pub fn save_auth_token(auth_token: AuthToken) {
+    match local_storage() {
+        None => warn!("unable to save auth_token"),
+        Some(storage) => {
+            let _ = storage
+                .set_item("auth_token", &auth_token.to_string())
+                .log_error("set auth_token");
+        }
+    }
 }
 
 // where should User ID / name come from?
